992f41f4b930b2e82ec6e8e227a51d0d307a6f33
mm1 <padetunes@Olamide.com>
2024-04-04 08:43:51 +0100

    adding mmabog project(9)

--- mmablogproject/settings.py
+++ mmablogproject/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY=env('SECRET_KEY')
 
 DEBUG=env('DEBUG')
 
-ALLOWED_HOSTS = ['https://mmablog.onrender.com']
+ALLOWED_HOSTS = ['https://mmablog.onrender.com', '*']
 RENDER_EXTERNAL_HOSTNAME=env('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
